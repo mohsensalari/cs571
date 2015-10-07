098fc613cfd9726cf82a0f8f7ddc5a40d6c43886
Mohsen Salari <apple@anon151.mathcs.emory.edu>
2015-10-07 14:21:39 -0400

    Stanford is now everything

--- src/main/java/edu/emory/mathcs/nlp/component/dep/feature/DEPFeatureTemplateStanford.java
+++ src/main/java/edu/emory/mathcs/nlp/component/dep/feature/DEPFeatureTemplateStanford.java
@@ -37,7 +37,86 @@ public class DEPFeatureTemplateStanford extends DEPFeatureTemplate
     protected void init()
     {
         BinUtils.LOG.info("FeatureTemplateStanford used\n");
-
+     // lemma features 
+     		add(new FeatureItem<>(Source.i, 0, Field.word_form));
+     		add(new FeatureItem<>(Source.j, 0, Field.word_form));
+     		add(new FeatureItem<>(Source.i, 0, Field.dependency_label));
+     		
+     		//template2
+     		// lemma features 
+    		add(new FeatureItem<>(Source.i, -1, Field.lemma));
+    		add(new FeatureItem<>(Source.i,  0, Field.lemma));
+    		add(new FeatureItem<>(Source.i,  1, Field.lemma));
+    		
+    		add(new FeatureItem<>(Source.j, -2, Field.lemma));
+    		add(new FeatureItem<>(Source.j, -1, Field.lemma));
+    		add(new FeatureItem<>(Source.j,  0, Field.lemma));
+    		add(new FeatureItem<>(Source.j,  1, Field.lemma));
+    		add(new FeatureItem<>(Source.j,  2, Field.lemma));
+    		
+    		add(new FeatureItem<>(Source.k,  1, Field.lemma));
+    		
+    		// pos features
+    		add(new FeatureItem<>(Source.i, -2, Field.pos_tag));
+    		add(new FeatureItem<>(Source.i, -1, Field.pos_tag));
+    		add(new FeatureItem<>(Source.i,  0, Field.pos_tag));
+    		add(new FeatureItem<>(Source.i,  1, Field.pos_tag));
+    		add(new FeatureItem<>(Source.i,  2, Field.pos_tag));
+    		
+    		add(new FeatureItem<>(Source.j, -2, Field.pos_tag));
+    		add(new FeatureItem<>(Source.j, -1, Field.pos_tag));
+    		add(new FeatureItem<>(Source.j,  0, Field.pos_tag));
+    		add(new FeatureItem<>(Source.j,  1, Field.pos_tag));
+    		add(new FeatureItem<>(Source.j,  2, Field.pos_tag));
+    		
+    		add(new FeatureItem<>(Source.k,  1, Field.pos_tag));
+    		add(new FeatureItem<>(Source.k,  2, Field.pos_tag));
+    		
+    		// valency features
+    		add(new FeatureItem<>(Source.i, 0, Field.valency, Direction.all));
+    		add(new FeatureItem<>(Source.j, 0, Field.valency, Direction.all));
+    		
+    		// 2nd-order features
+    		add(new FeatureItem<>(Source.i, Relation.h  , 0, Field.lemma));
+    		add(new FeatureItem<>(Source.i, Relation.lmd, 0, Field.lemma));
+    		add(new FeatureItem<>(Source.i, Relation.rmd, 0, Field.lemma));
+    		add(new FeatureItem<>(Source.j, Relation.lmd, 0, Field.lemma));
+    		
+    		add(new FeatureItem<>(Source.i, Relation.h  , 0, Field.pos_tag));
+    		add(new FeatureItem<>(Source.i, Relation.lmd, 0, Field.pos_tag));
+    		add(new FeatureItem<>(Source.i, Relation.rmd, 0, Field.pos_tag));
+    		add(new FeatureItem<>(Source.j, Relation.lmd, 0, Field.pos_tag));
+    		
+    		add(new FeatureItem<>(Source.i,               0, Field.dependency_label));
+    		add(new FeatureItem<>(Source.i, Relation.lns, 0, Field.dependency_label));
+    		add(new FeatureItem<>(Source.i, Relation.lmd, 0, Field.dependency_label));
+    		add(new FeatureItem<>(Source.i, Relation.rmd, 0, Field.dependency_label));
+    		add(new FeatureItem<>(Source.j, Relation.lmd, 0, Field.dependency_label));
+    		
+    		// 3rd-order features
+    		add(new FeatureItem<>(Source.i, Relation.h2  , 0, Field.lemma));
+    		add(new FeatureItem<>(Source.i, Relation.lmd2, 0, Field.lemma));
+    		add(new FeatureItem<>(Source.i, Relation.rmd2, 0, Field.lemma));
+    		add(new FeatureItem<>(Source.j, Relation.lmd2, 0, Field.lemma));
+    		
+    		add(new FeatureItem<>(Source.i, Relation.h2  , 0, Field.pos_tag));
+    		add(new FeatureItem<>(Source.i, Relation.lmd2, 0, Field.pos_tag));
+    		add(new FeatureItem<>(Source.i, Relation.rmd2, 0, Field.pos_tag));
+    		add(new FeatureItem<>(Source.j, Relation.lmd2, 0, Field.pos_tag));
+    		
+    		add(new FeatureItem<>(Source.i, Relation.h   , 0, Field.dependency_label));
+    		add(new FeatureItem<>(Source.i, Relation.lns2, 0, Field.dependency_label));
+    		add(new FeatureItem<>(Source.i, Relation.lmd2, 0, Field.dependency_label));
+    		add(new FeatureItem<>(Source.i, Relation.rmd2, 0, Field.dependency_label));
+    		add(new FeatureItem<>(Source.j, Relation.lmd2, 0, Field.dependency_label));
+    		
+    		// boolean features
+    		addSet(new FeatureItem<>(Source.i, 0, Field.binary));
+    		addSet(new FeatureItem<>(Source.j, 0, Field.binary));
+     		
+     		
+     		//end template 2
+     		
         // Single-word features
         // s1.w
         add(new FeatureItem<>(Source.i,  0, Field.lemma));
@@ -113,6 +192,5 @@ public class DEPFeatureTemplateStanford extends DEPFeatureTemplate
 
         // s2.t◦s1.w◦b1.t
         add(new FeatureItem<>(Source.i, 1, Field.pos_tag), new FeatureItem<>(Source.i, 0, Field.lemma),
-                new FeatureItem<>(Source.j,  0, Field.pos_tag));
-    }
+                new FeatureItem<>(Source.j,  0, Field.pos_tag));    }
 }
